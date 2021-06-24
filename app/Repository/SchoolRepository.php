54ac68227e670bb022ec27347011d35aa088b413
Asnex <asnex@yahoo.com>
2021-06-24 19:02:32 +0200

    Remove errors display

--- app/Repository/SchoolRepository.php
+++ app/Repository/SchoolRepository.php
@@ -4,9 +4,7 @@ use DB\Connect;
 use PDO;
 use PDOException;
 use SimpleXMLElement;
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 /**
  * Class SchoolRepository
  */
