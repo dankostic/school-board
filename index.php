c1513d1bdc11b8d193ec8f28c5ed1454d9e15696
Asnex <asnex@yahoo.com>
2021-06-24 15:58:57 +0200

    Install pecee/simple-router

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 require_once('vendor/autoload.php');
-
+use Pecee\SimpleRouter\SimpleRouter;
+use App\Controllers\SchoolController;
+SimpleRouter::setDefaultNamespace('\App\Controllers');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+SimpleRouter::get('/', [SchoolController::class, 'index']);
 use function Composer\Autoload\includeFile;
 
-includeFile('views/main.php');
+// Start the routing
+SimpleRouter::start();
 
+includeFile('views/main.php');
